59c47b699406d4577655d00090d183943053b5ef
yanislammari <yanis.lammari937@gmail.com>
2025-11-05 17:51:58 +0100

    Add Get Message by id request feature

--- src/resolvers/queries/message.query.ts
+++ src/resolvers/queries/message.query.ts
@@ -6,6 +6,17 @@ import MessageType from "../../types/message.type";
 const messageService = new MessageService();
 
 const MessageQuery = {
+  message: {
+    type: new GraphQLNonNull(MessageType),
+    args: {
+      id: {
+        type: new GraphQLNonNull(GraphQLID)
+      }
+    },
+    resolve: async (_parent: unknown, args: any, _context: any) => {
+      return await messageService.getMessageById(args.id);
+    }
+  },
   messagesOfConversation: {
     type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(MessageType))),
     args: {
